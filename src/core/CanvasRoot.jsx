bc23402a6b573cbaf6ab0a746df9043bfc093d16
KASHYAP MISTRY <128836459+Kashyap2way@users.noreply.github.com>
2026-01-07 13:52:25 +0530

    Update CanvasRoot.jsx

--- src/core/CanvasRoot.jsx
+++ src/core/CanvasRoot.jsx
@@ -1,10 +1,13 @@
 import { Canvas } from '@react-three/fiber'
+import { ScrollControls } from '@react-three/drei'
 import Background from '../background/Background'
 
 export default function CanvasRoot() {
   return (
     <Canvas camera={{ position: [0, 0, 6], fov: 60 }}>
-      <Background />
+      <ScrollControls pages={4}>
+        <Background />
+      </ScrollControls>
     </Canvas>
   )
 }
