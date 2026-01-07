a7172e89e41ee0e1f271f107f340d4a5534f0116
KASHYAP MISTRY <128836459+Kashyap2way@users.noreply.github.com>
2026-01-07 13:52:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
-// in App.jsx
 import CanvasRoot from './core/CanvasRoot'
-import { ScrollControls } from '@react-three/drei'
 
 export default function App() {
-  return (
-    <CanvasRoot>
-      <ScrollControls pages={4}>
-        {/* content handled inside ScrollScene */}
-      </ScrollControls>
-    </CanvasRoot>
-  )
+  return <CanvasRoot />
 }
