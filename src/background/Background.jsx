9399cde2f3d786089e6578c135e22a1899a763dd
KASHYAP MISTRY <128836459+Kashyap2way@users.noreply.github.com>
2026-01-07 12:14:11 +0530

    Refactor Background component into ScrollScene

--- src/background/Background.jsx
+++ src/background/Background.jsx
@@ -1,13 +1,16 @@
-import { Suspense } from 'react'
-import Particles from './Particles'
-import GradientFog from './GradientFog'
+import { useFrame, useThree } from '@react-three/fiber'
+import { useScroll } from '@react-three/drei'
+import { useRef } from 'react'
 
-export default function Background() {
-  return (
-    <Suspense fallback={null}>
-      <color attach="background" args={['#000']} />
-      <Particles />
-      <GradientFog />
-    </Suspense>
-  )
+export default function ScrollScene() {
+  const { camera } = useThree()
+  const scroll = useScroll()
+  const targetZ = useRef(0)
+
+  useFrame(() => {
+    // smooth scroll transition
+    targetZ.current = -scroll.offset * 40 // move camera over long scroll
+    camera.position.z += (targetZ.current - camera.position.z) * 0.05
+  })
+  return null
 }
