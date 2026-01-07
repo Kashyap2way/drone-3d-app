5c94af839beca5c7a6d2d153932b2611a75c7413
KASHYAP MISTRY <128836459+Kashyap2way@users.noreply.github.com>
2026-01-07 12:19:32 +0530

    Wrap CanvasRoot with ScrollControls component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+// in App.jsx
 import CanvasRoot from './core/CanvasRoot'
+import { ScrollControls } from '@react-three/drei'
 
 export default function App() {
-  return <CanvasRoot />
+  return (
+    <CanvasRoot>
+      <ScrollControls pages={4}>
+        {/* content handled inside ScrollScene */}
+      </ScrollControls>
+    </CanvasRoot>
+  )
 }
