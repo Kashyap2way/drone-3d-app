3fa2dea4a6a0b6b4a645ed97531f7edb0f2dcf0f
Kashyap2way <kashyap.mistry.btech2021@sitpune.edu.in.com>
2026-01-08 19:44:12 +0530

    4

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import './Menu.css'
+import { FiBell, FiMessageSquare, FiUser } from 'react-icons/fi'
 
 export default function Menu() {
   return (
-    <div className="menu-bar">
-      <div className="menu-item">About Developer</div>
-      <div className="menu-item">Projects</div>
-      <div className="menu-item">Submit Ideas</div>
-      <div className="menu-item">Contact</div>
+    <div className="menu-wrapper">
+      <div className="menu-bar">
+        <div className="menu-item">About Developer</div>
+        <div className="menu-item">Projects</div>
+        <div className="menu-item">Submit Ideas</div>
+        <div className="menu-item">Contact</div>
+      </div>
+
+      <div className="menu-icons">
+        <FiMessageSquare className="menu-icon" title="Messages" />
+        <FiBell className="menu-icon" title="Notifications" />
+        <FiUser className="menu-icon" title="Account" />
+      </div>
     </div>
   )
 }
