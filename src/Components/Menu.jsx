169deda04762b6ef8052c94b68edadbc5757397d
KASHYAP MISTRY <128836459+Kashyap2way@users.noreply.github.com>
2026-01-08 17:28:09 +0530

    Update Menu.jsx

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -4,10 +4,10 @@ import './Menu.css'
 export default function Menu() {
   return (
     <div className="menu-bar">
-      <div className="menu-item">About Developer</div>
-      <div className="menu-item">Projects</div>
-      <div className="menu-item">Submit Ideas</div>
-      <div className="menu-item">Contact</div>
+      <div className="menu-item">ABOUT</div>
+      <div className="menu-item">IDEAS</div>
+      <div className="menu-item">SUBMIT</div>
+      <div className="menu-item">CONTACT</div>
     </div>
   )
 }
